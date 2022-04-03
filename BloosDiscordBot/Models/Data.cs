dfa70a0c908852e748f1a7c2da0a393ca4e8e792
Bloo <17679976+bloofirephoenix@users.noreply.github.com>
2022-04-03 14:06:56 -0700

    ⌨️rewritten for c# and added more features

---
+++ BloosDiscordBot/Models/Data.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace BloosDiscordBot.Models;
+
+public class Data
+{
+    public static Data? DataInstance { get; set; }
+
+    public ulong? ReactionRoleMessageId { get; set; }
+
+    public static Data? GetData()
+    {
+        if (File.Exists("data.json"))
+        {
+            var d = JsonSerializer.Deserialize<Data>(File.ReadAllText("data.json"));
+            DataInstance = d;
+            return d;
+        }
+
+        return null;
+    }
+
+    public void SaveData()
+    {
+        File.WriteAllText("data.json", JsonSerializer.Serialize(this));
+    }
+}
